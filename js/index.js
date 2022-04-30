ad7692e26f564d86e647df4c41af28a57740139e
Hyeonwoo Kim <veraxanimus@naver.com>
2022-04-30 17:38:11 +0900

    2022043002

--- js/index.js
+++ js/index.js
@@ -12,26 +12,14 @@ let slides = document.querySelectorAll('.visual'), // 슬라이드 아이템
     pagerHTML = '';
 
 
-// // 슬라이드 아이템의 높이를 확인하여 부모의 높이로 지정
-// let topHeight = slides[0].offsetHeight;
-// function calculateTallsetSlide() {
-//   for(let i = 0; i < slideCount; i++) {
-//     if(slides[i].offsetHeight > topHeight) {
-//       topHeight = slides[i].offsetHeight;
-//     }
-//   }
-//   sliderContainer.style.height = topHeight + 'px';
-//   sliderWrapper.style.height = topHeight + 'px';
-// }
-// calculateTallsetSlide();
-
 // 슬라이드 아이템 자동으로 가로 정렬 및 갯수만큼 페이지네이션 버튼 자동 생성
 for(let i = 0; i < slideCount; i++) {
   slides[i].style.left = `${i * 100}%`;
-  pagerHTML += `<li data-index="${i}">${i + 1}</li>`;
+//   pagerHTML += `<li data-slide-index="${i}">${i + 1}</li>`;
+  pagerHTML += `<li data-slide-index="${i}"><img src="./images/circle.png"></li>`;
   pagerContainer.innerHTML = pagerHTML;
 }
-let pagerBtns = document.querySelectorAll('.pagerContainer li');
+const pagerBtns = document.querySelectorAll('.pagerContainer li');
 
 // 슬라이드 이동 함수
 function goToSlide(idx) {
@@ -80,7 +68,7 @@ function startAutoSlide() {
   timer = setInterval(() => {
     let nextIndex = (currentIndex + 1) % slideCount;
     goToSlide(nextIndex);
-  }, 4000);
+  }, 3000);
 }
 startAutoSlide();
 
@@ -94,10 +82,9 @@ sliderWrapper.addEventListener('mouseleave', () => {
 // 페이지네이션 클릭시 슬라이드 이동 함수 호출
 pagerBtns.forEach(pagerBtn => {
   pagerBtn.addEventListener('click', (event) => {
+      console.log(event.target);
     let pagerNum = event.target.dataset.index;
     goToSlide(pagerNum);
   });
 });
-
-
 });
