f161c5fbbfe7825784bd483de88c83326faa2613
Hyeonwoo Kim <veraxanimus@naver.com>
2022-05-01 16:22:40 +0900

    2022050102

--- js/seongbukdong.js
+++ js/seongbukdong.js
@@ -1,16 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
- 
-    //모달 창 
-    function onClick() {
-        document.querySelector('.modal_wrap').style.display ='block';
-        document.querySelector('.black_bg').style.display ='block';
-    }   
-    function offClick() {
-        document.querySelector('.modal_wrap').style.display ='none';
-        document.querySelector('.black_bg').style.display ='none';
+
+// Get the button that opens the modal
+const btn = document.querySelectorAll("button.modal-custom-button");
+
+// All page modals
+const modals = document.querySelectorAll('.modal-custom');
+
+// Get the <span> element that closes the modal
+const spans = document.getElementsByClassName("close-modal");
+
+// When the user clicks the button, open the modal
+for (let i = 0; i < btn.length; i++) {
+    btn[i].onclick = function (e) {
+        e.preventDefault();
+        modal = document.querySelector(e.target.getAttribute("href"));
+        modal.style.display = 'block';
     }
- 
-    document.querySelector('.modal_btn').addEventListener('click', onClick);
-    document.querySelector('.modal_close').addEventListener('click', offClick);
- 
+}
+
+// When the user clicks on <span> (x), close the modal
+for (let i = 0; i < spans.length; i++) {
+    spans[i].onclick = function () {
+        for (let index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+        }
+    }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target.classList.contains('modal-custom')) {
+        for (let index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+        }
+    }
+}
 });
