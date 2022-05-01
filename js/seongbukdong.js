641f1cfac63c5da152a32ee51cdf2d8e30676635
Hyeonwoo Kim <veraxanimus@naver.com>
2022-05-02 01:01:34 +0900

    2022050201

--- js/seongbukdong.js
+++ js/seongbukdong.js
@@ -50,12 +50,12 @@ for(let i = 0; i < tabList.length; i++){
       // 나머지 컨텐츠 display:none 처리
       contents[j].style.display = 'none';
     }
-
     // 버튼 관련 이벤트
     this.parentNode.classList.add('is_on');
 
     // 버튼 클릭시 컨텐츠 전환
     activeCont = this.getAttribute('href');
+
     document.querySelector(activeCont).style.display = 'block';
   });
 }
